72d959691c46efb187923eed363579a1eb51af23
Mutasim GreenLeaf <shoaib.dev70@gmail.com>
2025-10-05 13:59:06 +0500

    #

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:green_leaf/splash_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+n 
   runApp(const MyApp());
 }
 
